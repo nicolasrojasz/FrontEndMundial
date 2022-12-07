39b26840947dbd7039ffdf931dacf66dd2fb5a02
Nicolas <nicolas30rojas@gmaiil.com>
2022-12-07 09:51:42 -0500

    Avance inicial de diseño 'Navbar-Footer-Partidos'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { MatProgressBarModule } from '@angular/material/progress-bar'
+import { AppComponent } from './app.component'
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component'
+import { FooterComponent } from './Components/footer/footer.component'
 
-import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+
+import { MatCardModule } from '@angular/material/card'
+import { PartidosComponent } from './Components/partidos/partidos.component'
+import { LoginComponent } from './Components/login/login.component'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { Routes, RouterModule } from '@angular/router'
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JugadoresComponent } from './Components/jugadores/jugadores.component';
+const rutas: Routes = [
+  { path: 'partidos', component: PartidosComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'jugadores', component: JugadoresComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    PartidosComponent,
+    LoginComponent,
+    JugadoresComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatProgressBarModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(rutas),
+    MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
